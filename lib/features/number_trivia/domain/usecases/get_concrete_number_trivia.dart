f9d351841cc6ce684364dfa70011a1bf1583e8e1
Jatin Saini <sainijatin5940@gmail.com>
2024-04-29 07:59:24 +0530

    initial commit

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,6 @@
+import 'package:tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+  GetConcreteNumberTrivia(this.repository);
+}
